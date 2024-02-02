a8c344ae0002e84cecd61148f5d63b050572dea1
--global <emranffl.biz@gmail.com>
2024-02-03 00:19:01 +0600

    Implement deep object diffing function and update package name

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,73 @@
-function getMatchingValues<T, U>(obj1: T, obj2: U, depth = Infinity) {
-  const result: Partial<T> = {}
+// type NestedObject<T> = {
+//   [K in keyof T]: T[K] extends Array<infer U>
+//     ? U extends object
+//       ? Array<NestedObject<U>>
+//       : Array<T[K]>
+//     : T[K] extends object
+//       ? NestedObject<T[K]>
+//       : T[K]
+// }
 
-  Object.keys(obj2 as object).forEach((key) => {
-    // console.log(Array.isArray(obj1[key]) && Array.isArray(obj2[key]), depth)
+// function getMatchingValues<T extends object, U extends object>(
+//   obj1: T,
+//   obj2: U,
+//   depth = Infinity
+// ): NestedObject<U> {
+//   const result = {}
+//   const keys = Object.keys(obj1 as object) as Array<keyof T>
 
-    if (
-      Array.isArray(obj1[key]) &&
-      // && Array.isArray(obj2[key])
-      depth > 0
-    ) {
-      console.log(key)
+//   keys.forEach((key) => {
+//     // console.log(Array.isArray(obj1[key]) && Array.isArray(obj2[key]), depth)
 
-      result[key] = obj1[key].filter((val, index) => {
-        console.log(val, depth)
+//     if (
+//       Array.isArray(obj1[key]) &&
+//       // && Array.isArray(obj2[key])
+//       depth > 0
+//     ) {
+//       console.log(key)
+//       Object.assign(
+//         result,
+//         // result[key] =
+//         (obj1[key] as Array<any>).filter((val, index) => {
+//           console.log(val, depth)
 
-        return depth > 1 && val && typeof val === "object"
-          ? getMatchingValues(val, obj2[key][index], depth - 1)
-          : depth > 1 && val === obj1[key][index]
-      })
-    } else if (
-      !Array.isArray(obj1[key]) &&
-      // !Array.isArray(obj2[key]) &&
-      typeof obj1[key] === "object" &&
-      typeof obj2[key] === "object" &&
-      depth > 0
-    ) {
-      console.log(key)
+//           return depth > 1 && val && typeof val === "object"
+//             ? getMatchingValues(
+//                 val as T,
+//                 (obj2[key as unknown as keyof U] as Array<any>)[index],
+//                 depth - 1
+//               )
+//             : depth > 1 && val === (obj1[key] as Array<any>)[index]
+//         })
+//       )
+//     } else if (
+//       !Array.isArray(obj1[key]) &&
+//       // !Array.isArray(obj2[key]) &&
+//       typeof obj1[key] === "object" &&
+//       typeof obj2[key as unknown as keyof U] === "object" &&
+//       depth > 0
+//     ) {
+//       console.log(key)
 
-      result[key] = getMatchingValues(obj1[key] as T, obj2[key] as U, depth - 1)
-    } else if (obj1[key] && depth - 1 >= 0) {
-      console.log(key)
+//       // result[key] =
+//       Object.assign(
+//         result,
+//         getMatchingValues(
+//           obj1[key] as T,
+//           obj2[key as unknown as keyof U] as U,
+//           depth - 1
+//         )
+//       )
+//     } else if (obj1[key] && depth - 1 >= 0) {
+//       console.log(key)
 
-      result[key] = obj1[key]
-    }
-  })
+//       // result[key] = obj1[key]
+//       Object.assign(result, { [key]: obj1[key] })
+//     }
+//   })
 
-  return result
-}
+//   return result as NestedObject<U>
+// }
 
 const obj1 = {
   a: "Hello",
@@ -77,6 +108,86 @@ const obj2 = {
   },
 }
 
-const result = getMatchingValues(obj1, obj2, 5)
+// const result = getMatchingValues(obj1, obj2, 1)
 
-console.log(result)
+// console.log(result)
+
+// type MatchingValues<T, U> = {
+//   [K in keyof T & keyof U]: T[K] extends Array<infer TItem>
+//     ? U[K] extends Array<infer UItem>
+//       ? Array<MatchingValues<TItem, UItem>>
+//       : never
+//     : T[K] extends object
+//       ? U[K] extends object
+//         ? MatchingValues<T[K], U[K]>
+//         : never
+//       : T[K] extends U[K]
+//         ? T[K]
+//         : never
+// }
+
+type MatchingValues<T, U, D extends number = typeof Infinity> = {
+  [K in keyof T & keyof U]: T[K] extends Array<infer TItem>
+    ? U[K] extends Array<infer UItem>
+      ? D extends 0
+        ? never
+        : Array<
+            MatchingValues<TItem, UItem, D extends 0 ? 0 : D extends 1 ? 0 : D>
+          >
+      : never
+    : T[K] extends object
+      ? U[K] extends object
+        ? D extends 0
+          ? never
+          : MatchingValues<T[K], U[K], D extends 0 ? 0 : D extends 1 ? 0 : D>
+        : never
+      : T[K] extends U[K]
+        ? T[K]
+        : never
+}
+
+function getMatchingValues<T, U, D extends number>(
+  obj1: T extends object ? T : never,
+  obj2: U extends object ? U : never,
+  depth: D extends number ? D : typeof Infinity
+): MatchingValues<T, U, D> {
+  const result: Partial<T> = {}
+  const keys = Object.keys(obj1) as Array<keyof T>
+
+  keys.forEach((key) => {
+    if (
+      Array.isArray(obj1[key]) &&
+      Array.isArray(obj2[key as unknown as keyof U]) &&
+      depth > 0
+    ) {
+      result[key] = (obj1[key] as Array<any>).filter((val, index) => {
+        return depth > 1 && val && typeof val === "object"
+          ? getMatchingValues(
+              val as any,
+              (obj2[key as unknown as keyof U] as Array<any>)[index],
+              depth - 1
+            )
+          : depth > 1 && val === (obj1[key] as Array<any>)[index]
+      }) as any
+    } else if (
+      !Array.isArray(obj1[key]) &&
+      typeof obj1[key] === "object" &&
+      typeof obj2[key as unknown as keyof U] === "object" &&
+      depth > 0
+    ) {
+      result[key] = getMatchingValues(
+        obj1[key] as any,
+        obj2[key as unknown as keyof U] as any,
+        depth - 1
+      ) as any
+    } else if (obj1[key] && depth - 1 >= 0) {
+      result[key] = obj1[key]
+    }
+  })
+
+  return result
+}
+
+const result = getMatchingValues(obj1, obj2, 0)
+
+console.log(result.f)
